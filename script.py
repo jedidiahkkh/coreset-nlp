b3b1f70ac22250c3dfbc4f25c4aaa83922bef8b7
Jedidiah Koh <jedidiahkkh@gmail.com>
2024-03-11 13:07:56 +0000

    select data

--- script.py
+++ script.py
@@ -22,18 +22,53 @@ Algo = Literal["bert-base-uncased", "roberta-base"]
 Dataset = Literal["snli"]
 Selection = Literal["uniform", "full"]
 
+default_selection_args = dict(
+    selection_epochs=40,
+    selection_momentum=0.9,
+    selection_weight_decay=5e-4,
+    selection_optimizer="SGD",
+    selection_nesterov=True,
+    selection_lr=0.1,
+    selection_test_interval=1,
+    selection_test_fraction=1.0,
+    balance=True,
+)
+
+unknown_args = dict(
+    submodular="GraphCut",
+    submodular_greedy="LazyGreedy",
+    uncertainty="Entropy",
+)
+
 
 def train(
     algo: Algo = "bert-base-uncased",
     dataset_name: Dataset = "snli",
     epochs: int = 3,
-    selection: str = "full",
+    selection: str = "Uniform",
+    fraction: float = 0.1,
     reduced: bool = False,
 ) -> int:
+    logger.info(
+        "Args received %s",
+        {
+            "algo": algo,
+            "dataset_name": dataset_name,
+            "epochs": epochs,
+            "selection": selection,
+            "fraction": fraction,
+            "reduced": reduced,
+        },
+    )
 
     dataset = load_dataset(dataset_name)
     model = AutoModelForSequenceClassification.from_pretrained(algo, num_labels=3)
     tokenizer = AutoTokenizer.from_pretrained(algo)
+
+    if reduced:
+        dataset = dataset.shuffle(SEED)
+        dataset["train"] = dataset["train"].select(range(100))
+        dataset["validation"] = dataset["validation"].select(range(100))
 
     def encode(examples):
         return tokenizer(examples["premise"], examples["hypothesis"], truncation=True)
@@ -47,11 +82,6 @@ def train(
     )
 
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-    if reduced:
-        dataset = dataset.shuffle(SEED)
-        dataset["train"] = dataset["train"].select(range(10))
-        dataset["validation"] = dataset["validation"].select(range(10))
 
     def compute_metrics(p):
         pred, labels = p
@@ -74,7 +104,34 @@ def train(
         evaluation_strategy="epoch",
         num_train_epochs=epochs,
         seed=SEED,
+        report_to="none",  # fix for some bug where tensorboard is added as an integration
     )
+
+    # Coreset selection ========================================================
+
+    # patch for selection method to access
+    dataset["train"].classes = np.unique(dataset["train"]["label"])
+    dataset["train"].targets = np.array(dataset["train"]["label"])
+
+    selection_args = dict(
+        epochs=default_selection_args["selection_epochs"],
+        selection_method=unknown_args["uncertainty"],
+        balance=default_selection_args["balance"],
+        greedy=unknown_args["submodular_greedy"],
+        function=unknown_args["submodular"],
+    )
+    method = methods.__dict__[selection](
+        dataset["train"],
+        default_selection_args,
+        fraction,
+        SEED,
+        **selection_args,
+    )
+    subset = method.select()
+
+    dataset["train"] = dataset["train"].select(subset["indices"])
+    logger.info(f"Total size of training set: {len(dataset['train'])}")
+    # =============================================================================
 
     trainer = Trainer(
         model=model,
